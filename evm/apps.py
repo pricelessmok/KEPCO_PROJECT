bd58fbb00d97e0dfd1f0e2f45fa107b247b9afb5
pricelessmok <pricelessmok@gmail.com>
2017-11-04 21:39:04 +0900

    start

---
+++ evm/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EvmConfig(AppConfig):
+    name = 'evm'
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pricelessmok/Desktop/kepco/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
